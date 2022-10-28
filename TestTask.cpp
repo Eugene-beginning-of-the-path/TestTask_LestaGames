11f0c30095103a84a8cdc77390f55198506b0daa
Eugene <69599396+Eugene-beginning-of-the-path@users.noreply.github.com>
2022-10-28 14:18:20 +0300

    Task 2 is complited!

--- TestTask.cpp
+++ TestTask.cpp
@@ -1,18 +1,9 @@
-﻿#include <iostream>
-void task1();
+﻿void task1();
 void task2();
-
-std::string foo()
-{
-    return "-1" + std::string(std::numeric_limits<int>::digits, '0');
-}
 
 int main()
 {
-    int a = -16;
-    if (!(-16))
-        std::cout << '+';
-    //std::cout << a;
+    task2();
 
 
 }
--- ToBinary.cpp
+++ ToBinary.cpp
@@ -3,37 +3,39 @@
 bool isEven(int value)
 {
 	std::string buffer;
+	bool negative = false;
 	buffer.reserve(std::numeric_limits<int>::digits + 1); //добавляем +1 для представления 
 													//отрицательного числа в бинарном виде
 	//по методу "two's component"
-	if (!value)
+	if (value < 0)
+	{
 		value = -value;
+		negative = true;
+	}
 
 	do
 	{
 		buffer += char('0' + value % 2);
 		value /= 2;
-	} while (value);
+	} while (value); //в buffer будет хранится двоичное представление десятичного числа
+					 //в обратной последовательности!! Т.е. стоку нужно перевернуть для
+					 //корректного отображения в двоичном виде
 
-	if (value >= 0)
-	{
-		
-		
-		//std::cout << std::endl << buffer << std::endl;
+	if (negative)
+		buffer += '-';
 
-		if (buffer[0] == '0') 
-			return true;	//число четное
-		else
-			return false;	//число нечетное
+	//с помощью итераторов идем с конца строки --> в начало, чтобы отобразить двоичное представление
+	for (auto i = buffer.crbegin(); i < buffer.crend(); i++)
+		std::cout << *i;
+	std::cout << std::endl;
 
-	}
+	if (buffer[0] == '0')
+		return true;	//число четное
 	else
-	{
-
-	}
+		return false;	//число нечетное
 }
 
 void task2()
 {
-	std::cout << isEven(16);
+	(isEven(-157)) ? (std::cout << "The number is even") : (std::cout << "The number is not even");
 }
