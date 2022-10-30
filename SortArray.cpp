0e35eae2e72586e40728f9d6b8cf9ec605c672e6
Eugene-beginning-of-the-path <EvgeniiDStudent@yandex.ru>
2022-10-30 14:39:56 +0300

    Some changes of comments

---
+++ SortArray.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 5;
+
+void Merge(int arr[], int left, int right)
+{
+	int i = left; //для интерации по левой последовательности 
+	int middle = left + (right - left) / 2;
+	int j = middle + 1; //для интерации по правой последовательности 
+	int t = 0; //счётчик для arrMerged
+	int arrMerged[SIZE]; //сливаем элементы из разных последовательностей 
+
+	while (i <= middle && j <= right)
+	{
+
+		if (arr[i] <= arr[j])
+		{
+			arrMerged[t] = arr[i];
+			i++;
+		}
+		else
+		{
+			arrMerged[t] = arr[j];
+			j++;
+		}
+		t++;
+	}
+
+	while (i <= middle)
+	{
+		arrMerged[t] = arr[i];
+		i++;
+		t++;
+	}
+
+	while (j <= right)
+	{
+		arrMerged[t] = arr[j];
+		j++;
+		t++;
+	}
+
+	for (i = 0; i < t; i++)
+		arr[left + i] = arrMerged[i];
+}
+//							 0			2
+void Sort(int* arr, int left, int right) //left and right - must be index of elements
+{
+	if (left < right) ///если в последовательности хотя бы 2 элемента и больше (при 1 эл-нте не пройдет)
+		if (right - left == 1) //если в последовательности ровно 2 элемента
+		{
+			if (arr[left] > arr[right])
+			{
+				int temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+			}
+		}
+		else
+		{
+			int middle = (left + right) / 2; //здесь тоже определяется индекс элемента
+
+			Sort(arr, left, middle); 
+			Sort(arr, middle + 1, right); 
+
+			Merge(arr, left, right); 
+		}
+
+}
+ 
+static void display_arr(int* arr)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		std::cout << arr[i] << "\t";
+	}
+	std::cout << std::endl;
+}
+
+void start_merge_sort()
+{
+	int arr[SIZE] = { 8, 2, 1, 7, 3 };
+	display_arr(arr);
+	Sort(arr, 0, SIZE - 1);
+	display_arr(arr);
+}
--- ToBinary.cpp
+++ ToBinary.cpp
@@ -6,7 +6,7 @@ bool isEven(int value)
 	bool negative = false;
 	buffer.reserve(std::numeric_limits<int>::digits + 1); //добавляем +1 для представления 
 													//отрицательного числа в бинарном виде
-	//по методу "two's component"
+													//по методу "two's component"
 	if (value < 0)
 	{
 		value = -value;
@@ -19,15 +19,17 @@ bool isEven(int value)
 		value /= 2;
 	} while (value); //в buffer будет хранится двоичное представление десятичного числа
 					 //в обратной последовательности!! Т.е. стоку нужно перевернуть для
-					 //корректного отображения в двоичном виде
+					 //корректного отображения в двоичном виде, но т.к. мы показывать двоичное
+					 //представление мы не будем, поэтому можно не переворачивать, но
+					 //держать в уме, что начало у нас - в конце строки buffer
 
 	if (negative)
 		buffer += '-';
 
-	//с помощью итераторов идем с конца строки --> в начало, чтобы отобразить двоичное представление
-	for (auto i = buffer.crbegin(); i < buffer.crend(); i++)
-		std::cout << *i;
-	std::cout << std::endl;
+	////с помощью итераторов идем с конца строки --> в начало, чтобы отобразить двоичное представление
+	//for (auto i = buffer.crbegin(); i < buffer.crend(); i++)
+	//	std::cout << *i;
+	//std::cout << std::endl;
 
 	if (buffer[0] == '0')
 		return true;	//число четное
@@ -37,5 +39,6 @@ bool isEven(int value)
 
 void task2()
 {
-	(isEven(-157)) ? (std::cout << "The number is even") : (std::cout << "The number is not even");
+	(isEven(17)) ? (std::cout << "The number is even") : 
+		(std::cout << "The number is not even");
 }
